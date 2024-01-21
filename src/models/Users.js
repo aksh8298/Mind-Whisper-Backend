820794b4ed46b95414b4cea4ca95cce7973dc2e6
aksh8298 <akankshasingh0035@gmail.com>
2024-01-20 16:24:15 -0800

    First Release

---
+++ src/models/Users.js
@@ -0,0 +1,33 @@
+const mongoose= require("mongoose")
+const bcrypt=require("bcrypt")
+
+
+const user= new mongoose.Schema({
+name:{
+    type:String,
+    required:false
+},
+phone:{
+    type:Number,
+    required:false
+},
+email:{
+    type:String,
+    required:true
+},
+
+hash_password:{
+    type:String,
+    required:true
+}
+})
+user.virtual("password").set(function(password){
+    this.hash_password=bcrypt.hashSync(password,10)
+})
+user.methods={
+    authenticate:function(password){
+        return bcrypt.compareSync(password,this.hash_password)
+        
+    }
+}
+module.exports=mongoose.model("Users",user);
